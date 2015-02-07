642b96f3b393e4fdf5a5243f663b4d0d9032fbcd
Dallas Gutauckis <dallas@gutauckis.com>
2015-02-07 15:21:41 -0500

    Adding toString/equals/hashCode to SpreedlyCreditCard

--- src/main/java/cc/protea/spreedly/model/SpreedlyCreditCard.java
+++ src/main/java/cc/protea/spreedly/model/SpreedlyCreditCard.java
@@ -247,5 +247,100 @@ public class SpreedlyCreditCard {
         this.data = data;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        SpreedlyCreditCard that = (SpreedlyCreditCard) o;
+
+        if (address1 != null ? !address1.equals(that.address1) : that.address1 != null) return false;
+        if (address2 != null ? !address2.equals(that.address2) : that.address2 != null) return false;
+        if (cardType != that.cardType) return false;
+        if (city != null ? !city.equals(that.city) : that.city != null) return false;
+        if (country != null ? !country.equals(that.country) : that.country != null) return false;
+        if (data != null ? !data.equals(that.data) : that.data != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (fullName != null ? !fullName.equals(that.fullName) : that.fullName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (month != null ? !month.equals(that.month) : that.month != null) return false;
+        if (number != null ? !number.equals(that.number) : that.number != null) return false;
+        if (phoneNumber != null ? !phoneNumber.equals(that.phoneNumber) : that.phoneNumber != null) return false;
+        if (shippingAddress1 != null ? !shippingAddress1.equals(that.shippingAddress1) : that.shippingAddress1 != null) return false;
+        if (shippingAddress2 != null ? !shippingAddress2.equals(that.shippingAddress2) : that.shippingAddress2 != null) return false;
+        if (shippingCity != null ? !shippingCity.equals(that.shippingCity) : that.shippingCity != null) return false;
+        if (shippingCountry != null ? !shippingCountry.equals(that.shippingCountry) : that.shippingCountry != null) return false;
+        if (shippingPhoneNumber != null ? !shippingPhoneNumber.equals(that.shippingPhoneNumber) : that.shippingPhoneNumber != null) return false;
+        if (shippingState != null ? !shippingState.equals(that.shippingState) : that.shippingState != null) return false;
+        if (shippingZip != null ? !shippingZip.equals(that.shippingZip) : that.shippingZip != null) return false;
+        if (state != null ? !state.equals(that.state) : that.state != null) return false;
+        if (trackData != null ? !trackData.equals(that.trackData) : that.trackData != null) return false;
+        if (verificationValue != null ? !verificationValue.equals(that.verificationValue) : that.verificationValue != null) return false;
+        if (year != null ? !year.equals(that.year) : that.year != null) return false;
+        if (zip != null ? !zip.equals(that.zip) : that.zip != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        result = 31 * result + (trackData != null ? trackData.hashCode() : 0);
+        result = 31 * result + (month != null ? month.hashCode() : 0);
+        result = 31 * result + (year != null ? year.hashCode() : 0);
+        result = 31 * result + (verificationValue != null ? verificationValue.hashCode() : 0);
+        result = 31 * result + (cardType != null ? cardType.hashCode() : 0);
+        result = 31 * result + (address1 != null ? address1.hashCode() : 0);
+        result = 31 * result + (address2 != null ? address2.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (zip != null ? zip.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        result = 31 * result + (shippingAddress1 != null ? shippingAddress1.hashCode() : 0);
+        result = 31 * result + (shippingAddress2 != null ? shippingAddress2.hashCode() : 0);
+        result = 31 * result + (shippingCity != null ? shippingCity.hashCode() : 0);
+        result = 31 * result + (shippingState != null ? shippingState.hashCode() : 0);
+        result = 31 * result + (shippingZip != null ? shippingZip.hashCode() : 0);
+        result = 31 * result + (shippingCountry != null ? shippingCountry.hashCode() : 0);
+        result = 31 * result + (shippingPhoneNumber != null ? shippingPhoneNumber.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (data != null ? data.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "SpreedlyCreditCard{" +
+                       "firstName='" + firstName + '\'' +
+                       ", lastName='" + lastName + '\'' +
+                       ", fullName='" + fullName + '\'' +
+                       ", number='" + number + '\'' +
+                       ", trackData='" + trackData + '\'' +
+                       ", month=" + month +
+                       ", year=" + year +
+                       ", verificationValue='" + verificationValue + '\'' +
+                       ", cardType=" + cardType +
+                       ", address1='" + address1 + '\'' +
+                       ", address2='" + address2 + '\'' +
+                       ", city='" + city + '\'' +
+                       ", state='" + state + '\'' +
+                       ", zip='" + zip + '\'' +
+                       ", country='" + country + '\'' +
+                       ", phoneNumber='" + phoneNumber + '\'' +
+                       ", shippingAddress1='" + shippingAddress1 + '\'' +
+                       ", shippingAddress2='" + shippingAddress2 + '\'' +
+                       ", shippingCity='" + shippingCity + '\'' +
+                       ", shippingState='" + shippingState + '\'' +
+                       ", shippingZip='" + shippingZip + '\'' +
+                       ", shippingCountry='" + shippingCountry + '\'' +
+                       ", shippingPhoneNumber='" + shippingPhoneNumber + '\'' +
+                       ", email='" + email + '\'' +
+                       ", data=" + data +
+                       '}';
+    }
 }
